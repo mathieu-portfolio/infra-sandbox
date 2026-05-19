9cc51683fe2e5e644350e3560e986cb816bba6d2
mathieu-portfolio <mathieu.sterlin+portfolio@gmail.com>
2026-05-19 16:37:33 +0200

    Project cleanup

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cmake --preset app-debug
-cmake --build --preset app-debug
--- tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cmake --preset tests-debug
-cmake --build --preset tests-debug
-ctest --preset tests-debug
